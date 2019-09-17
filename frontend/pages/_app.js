d4bec4f84ecd724bdb30c15e41cd868de6d25992
David <oromis@elefantracing.de>
2019-09-17 20:39:00 +0200

    Initial commit

---
+++ frontend/pages/_app.js
@@ -0,0 +1,33 @@
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+
+
+export default class SimRacingApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossOrigin="anonymous"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+      </>
+    );
+  }
+}
